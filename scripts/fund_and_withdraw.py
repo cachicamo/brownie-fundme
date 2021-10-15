79ea726515039a7f17a54763eaf2376635892f9a
cachicamo <35459697+cachicamo@users.noreply.github.com>
2021-10-14 21:58:08 -0400

    first commit

---
+++ scripts/fund_and_withdraw.py
@@ -0,0 +1,25 @@
+from brownie import FundMe
+
+from scripts.helpful_scripts import get_account
+
+
+def fund():
+    fund_me = FundMe[-1]
+    account = get_account()
+    entrance_fee = fund_me.getEntranceFee()
+    print(f"FundMe contract address {fund_me.address}")
+    print(f"The current entry fee is {entrance_fee}")
+    print("Funding...")
+    fund_me.fund({"from": account, "value": entrance_fee})
+
+
+def withdraw():
+    fund_me = FundMe[-1]
+    account = get_account()
+    fund_me.withdraw({"from": account})
+
+
+# 0.025000000000000000
+def main():
+    fund()
+    withdraw()
